d0a751604a71ddeac7e147b478a97e4a518bb227
Brayan  Saa <89494449+BrayanSaa@users.noreply.github.com>
2024-05-06 04:11:50 -0500

    Add files via upload

---
+++ ejerciciosPractica/src/main/java/org/ejercicio1/Main.java
@@ -0,0 +1,24 @@
+package org.ejercicio1;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String directoryPath = "D:\\Proyectos\\programacion\\ejerciciosPractica\\src\\main\\java\\org\\ejercicio1\\archivos"; // Reemplaza con la ruta de tu directorio
+        String searchWord = "cosa"; // La palabra que quieres buscar
+
+        File directory = new File(directoryPath);
+        if (!directory.isDirectory()) {
+            System.out.println("El directorio especificado no es válido.");
+            return;
+        }
+
+        // Crear y lanzar dos hilos para buscar las palabras
+        SearchThread thread1 = new SearchThread(directory, "cosa");
+        SearchThread thread2 = new SearchThread(directory, "otraPalabra");
+
+        thread1.start();
+        thread2.start();
+    }
+}
